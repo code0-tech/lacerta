db0a0e2dd4340cdd9a994935b2bee122c625da5b
Nicuschgifthub <nicolas260504@mail.com>
2025-12-17 20:55:41 +0100

    added _ and # to be ignored

--- src/dc-client/language-file-check.js
+++ src/dc-client/language-file-check.js
@@ -9,12 +9,12 @@ const checkLanguage = (exampleJson, langJson, langName) => {
             const commandTexts = Object.keys(langJson[exampleCommandKey]);
 
             exampleJson[exampleCommandKey].forEach(commandText => {
-                if (!commandTexts.includes(commandText) && !commandText.startsWith('#')) { // Command text with # is just for one language
+                if (!commandTexts.includes(commandText) && !commandText.startsWith('#') && !commandText.startsWith('_')) {
                     console.log(`[Lang Check] In file ${langName}, text for /${exampleCommandKey} with text "${commandText}" is missing.`, Constants.CONSOLE.ERROR);
                 }
             });
         } else {
-            if (!exampleCommandKey.startsWith('#')) {
+            if (!exampleCommandKey.startsWith('#') && !exampleCommandKey.startsWith('_')) {
                 console.log(`[Lang Check] In file ${langName}, text for /${exampleCommandKey} is missing.`, Constants.CONSOLE.ERROR);
             }
         }
