1590848fe32d0a323d939648cc22e7a3691155ba
Nicuschgifthub <nicolas260504@mail.com>
2025-12-17 13:44:35 +0100

    added the check for invitation gifts

--- src/dc-guild/invite-tracker.js
+++ src/dc-guild/invite-tracker.js
@@ -1,10 +1,30 @@
-const Constants = require('../../data/constants');
-const { Collection } = require('discord.js');
 const { MongoUser, MongoUserConsts } = require('../mongo/MongoUser');
+const Constants = require('../../data/constants');
+const config = require('./../../config.json');
+const { Collection } = require('discord.js');
+const DC = require('../singleton/DC');
 
 const inviteCache = new Collection();
 
-const saveUserIfValidInvite = async (inviterId, invitedId) => {
+const checkForInvitesGift = async (inviterMongo, inviterId, invitedId, client) => {
+    if ((await inviterMongo.getInviteStats()).real < config.functions.invites.actions.gift.realInvites) return;
+
+    if (await inviterMongo.getFlag(MongoUserConsts.FLAGS.INVITE_GIFT_RECEIVED) == true) return;
+
+    const member = await DC.codeZeroMemberById(inviterId, client);
+
+    DC.memberAddRoleId(member, config.functions.invites.actions.gift.roleId);
+
+    inviterMongo.setFlag(MongoUserConsts.FLAGS.INVITE_GIFT_RECEIVED, true);
+
+    console.log(`[INVITE-TRACKER] Member ${inviterId} was gifted "INVITE_GIFT".`, Constants.CONSOLE.GOOD);
+}
+
+const checkForAction = (inviterMongo, inviterId, invitedId, client) => {
+    checkForInvitesGift(inviterMongo, inviterId, invitedId, client);
+}
+
+const saveUserIfValidInvite = async (inviterId, invitedId, client) => {
     try {
         const inviter = new MongoUser(inviterId);
         await inviter.init();
@@ -12,16 +32,18 @@ const saveUserIfValidInvite = async (inviterId, invitedId) => {
         const existingInviterDoc = await inviter.findOriginalInviter(invitedId);
 
         if (existingInviterDoc) {
-            console.log(`[INVITE-TRACKER] Member ${invitedId} joined again. Already credited to ${existingInviterDoc.id}. Only incrementing Total.`, Constants.CONSOLE.INFO);
+            console.log(`[INVITE-TRACKER] Member ${invitedId} rejoined. Already credited to ${existingInviterDoc.id}.`, Constants.CONSOLE.INFO);
 
             await inviter.updateInvitesBy(MongoUserConsts.INVITES.TYPES.TOTAL, 1);
         } else {
-            console.log(`[INVITE-TRACKER] New unique join! Crediting ${inviterId} for inviting ${invitedId}.`, Constants.CONSOLE.INFO);
+            console.log(`[INVITE-TRACKER] Crediting ${inviterId} for inviting ${invitedId}.`, Constants.CONSOLE.INFO);
 
             await inviter.updateInvitesBy(MongoUserConsts.INVITES.TYPES.TOTAL, 1);
             await inviter.updateInvitesBy(MongoUserConsts.INVITES.TYPES.REAL, 1);
             await inviter.addInvitedMember(invitedId);
         }
+
+        checkForAction(inviter, inviterId, invitedId, client);
     } catch (err) {
         console.log(`[INVITE-TRACKER] Error in saveUserIfValidInvite`, Constants.CONSOLE.ERROR);
     }
@@ -54,7 +76,7 @@ const start = async (client) => {
                 const inviter = usedInvite.inviter;
                 console.log(`[INVITE-TRACKER] ${member.user.tag} joined using code ${usedInvite.code} from ${inviter.id} ${inviter?.tag || 'Unknown'}`, Constants.CONSOLE.WORKING);
 
-                saveUserIfValidInvite(inviter.id, member.id);
+                saveUserIfValidInvite(inviter.id, member.id, client);
             } else {
                 console.log(`[INVITE-TRACKER] ${member.user.tag} joined, but no invite match found (Vanity URL or OAuth2 likely).`, Constants.CONSOLE.ERROR);
             }
