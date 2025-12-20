44d9c5490d4f9f4074d298ac96c21a49abd83a39
Nicuschgifthub <nicolas260504@mail.com>
2025-12-20 15:29:25 +0100

    mongo config changed to have a more stable connection between the server and this client

--- src/start-up/mongo-setup.js
+++ src/start-up/mongo-setup.js
@@ -1,20 +1,33 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const Constants = require('./../../data/constants');
-const { MongoClient } = require('mongodb');
 
-const mongoClient = new MongoClient(process.env.MONGO_URL);
+const mongoClient = new MongoClient(process.env.MONGO_URL, {
+    maxPoolSize: 10,
+    minPoolSize: 2,
+    connectTimeoutMS: 10000,
+    // socketTimeoutMS: 45000,
+    heartbeatFrequencyMS: 10000,
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 const connect = async () => {
-    mongoClient.connect()
-        .then(() => {
-            console.log('[MongoDb] Connected to MongoDB', Constants.CONSOLE.GOOD);
-            return true;
-        })
-        .catch((err) => {
-            console.error('[MongoDb] Error connecting to MongoDB:', err);
-            process.exit(1);
+    try {
+        await mongoClient.connect();
+        console.log('[MongoDb] Connected to MongoDB', Constants.CONSOLE.GOOD);
+
+        mongoClient.on('error', (err) => {
+            console.log(`[MongoDb] Connection lost!: ${err}`, Constants.CONSOLE.ERROR);
         });
+
+    } catch (err) {
+        console.error('[MongoDb] Fatal Error during initial connect:', err);
+        process.exit(1);
+    }
 };
 
 global.mongoClient = mongoClient;
-
 module.exports = { connect };
