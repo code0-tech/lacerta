a13f033980047d7fa1997319a8392494f4003db1
Nicuschgifthub <nicolas260504@mail.com>
2025-12-20 19:29:41 +0100

    changed file name

--- src/dc-guild/fetch-channels.js
+++ src/dc-guild/fetch-channels.js
@@ -1,8 +1,9 @@
 const Constants = require('../../data/constants');
 const config = require('../../config.json');
+const DC = require('../singleton/DC');
 
 const fetch = async (client) => {
-    const guild = await client.guilds.fetch(config.serverid);
+    const guild = await DC.guildById(config.serverid, client);
 
     const channels = await guild.channels.fetch();
     console.log(`[Pre-Fetch] Fetched ${channels.size} channels in guild ${guild.name}.`, Constants.CONSOLE.FOUND);
