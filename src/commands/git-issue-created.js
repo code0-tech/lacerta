35dbf36033d966471af0c2ef8fb17b21b49ce3ca
Nicuschgifthub <nicolas260504@gmail.com>
2026-03-19 18:57:25 +0100

    SlashCommandBuilder text changed

--- src/commands/git-issue-created.js
+++ src/commands/git-issue-created.js
@@ -7,13 +7,13 @@ const data = new SlashCommandBuilder()
     .setName('git-issue-created')
     .setDescription('Create a info message with a Github Link to the issues that was created.')
     .setDescriptionLocalizations({
-        de: 'Send eine Info Nachricht mit dem Link zum Github issues welches erstellt wurde.',
+        de: 'Send eine Info Nachricht mit dem Link zum Github issue welches erstellt wurde.',
     })
     .addStringOption(option =>
         option.setName('link')
             .setDescription('Github Link')
             .setDescriptionLocalizations({
-                de: 'Der Github Link',
+                de: 'Github Link',
             })
             .setRequired(true))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
